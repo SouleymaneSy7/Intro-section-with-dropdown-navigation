14449e2b6d93c56edf08acc6396a6673ceaf4368
Souleymane Sy <souleymanesyservices@gmail.com>
2022-12-10 23:51:19 +0100

    initial commit

--- App/JS/app.js
+++ App/JS/app.js
@@ -2,20 +2,14 @@
 
 const dropdown = document.querySelectorAll('.drop-down-container');
 
-dropdown.forEach( (dropdownItems) => {
+dropdown.forEach((dropdownItems) => {
     dropdownItems.addEventListener('click', () => {
         dropdownItems.classList.toggle('active');
     })
 });
 
-// hamburger Active
 
-// const hamburger = document.querySelector('.hamburger-menu'); 
-
-// hamburger.addEventListener('click', () => {
-//     hamburger.classList.toggle('active');
-// });
-
+// Hamburger Menu
 
 const showMenu = (hamburgerMenu, navItems) => {
     const hamburger = document.querySelector(hamburgerMenu);
